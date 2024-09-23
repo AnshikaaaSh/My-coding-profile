3d815a7cfd6e69a3e1e0758e0ac3e5ca0d6ec466
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-09-23 12:19:34 +0530

    Time: 10 ms (42.19%), Space: 48.6 MB (11.04%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,16 +3,26 @@ class Solution {
         int n=g.length, m=s.length,c=0;
         Arrays.sort(g);
         Arrays.sort(s);
-        for(int i=0,j=0;i<n;){
-            if(j>=m) break;
-            if(g[i]>s[j]){
+        // for(int i=0,j=0;i<n;){
+        //     if(j>=m) break;
+        //     if(g[i]>s[j]){
                 
-                j++;
-            }
-            else{
-                j++;i++;
+        //         j++;
+        //     }
+        //     else{
+        //         j++;i++;
+        //         c++;
+        //     }
+        // }
+
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                i++;j++;
                 c++;
+                continue;
             }
+            else j++;
         }
         return c;
     }
