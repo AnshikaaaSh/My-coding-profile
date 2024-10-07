1f4d8eed5d82bfe99dab0cf3113dda966b04589f
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-10-07 23:47:43 +0530

    Time: 2 ms (99.76%), Space: 49.7 MB (33.8%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void solve(int r, int c, char[][] grid){
+        int n = grid.length, m = grid[0].length;
+        if(r<0 || r>=n || c<0 || c>=m || grid[r][c] == '0') return;
+
+        grid[r][c] = '0';
+        solve(r+1,c,grid);
+        solve(r-1,c,grid);
+        solve(r,c+1,grid);
+        solve(r,c-1,grid);
+
+    }
+    
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length, ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    solve(i,j,grid);
+                }
+            }
+        }
+        return ans;
+    }
+}
