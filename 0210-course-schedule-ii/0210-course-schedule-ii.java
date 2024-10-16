2872f57e4c7ddc5b6dd7bfc5d3d09b214c34dbd2
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-10-16 20:22:56 +0530

    Time: 5 ms (78.91%), Space: 45.9 MB (39.22%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,36 +1,33 @@
 class Solution {
-    public void fun(int[] indeg, int[][] pre){
-        for(int i=0;i<pre.length;i++) indeg[pre[i][0]]++;
-    }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
         int[] indeg=new int[numCourses];
-        fun(indeg, prerequisites);
-        Queue<Integer> q=new LinkedList<>();
+        int[] res=new int[numCourses];
 
-        for(int i=0;i<indeg.length;i++){
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] p:prerequisites){
+            adj.get(p[1]).add(p[0]);
+            indeg[p[0]]++;
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
             if(indeg[i]==0) q.add(i);
         }
-
-        ArrayList<Integer> arr=new ArrayList<>();
-
+        int in=0;
         while(!q.isEmpty()){
-            int curr=q.poll();
-            arr.add(curr);
-            for(int i=0;i<prerequisites.length;i++){
-                if(prerequisites[i][1]==curr){
-                    indeg[prerequisites[i][0]]--;
-                    if(indeg[prerequisites[i][0]]==0) q.add(prerequisites[i][0]);
-                }
+            int course=q.poll();
+            res[in++]=course;
+            for(int nc: adj.get(course)){
+                indeg[nc]--;
+                if(indeg[nc]==0) q.add(nc);
             }
         }
-        int s=arr.size();
-        if(s!=numCourses) return new int[0];
+        if(in!=numCourses) return new int[0];
 
-        int[] res=new int[s];
-
-        for(int i=0;i<s;i++){
-            res[i]=arr.get(i);
-        }
         return res;
     }
 }
