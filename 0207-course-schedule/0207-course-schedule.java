b34fd754861ade3ebaea5af2804e03114b07c0d5
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-10-17 00:16:32 +0530

    Time: 7 ms (43.52%), Space: 45.3 MB (44.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
         int[] indeg=new int[numCourses];
-        //int[] res=new int[numCourses];
 
         for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
 
@@ -18,7 +17,6 @@ class Solution {
         while(!q.isEmpty()){
             int course=q.poll();
             in++;
-            //res[in++]=course;
             for(int nc:adj.get(course)){
                 indeg[nc]--;
                 if(indeg[nc]==0) q.add(nc);
