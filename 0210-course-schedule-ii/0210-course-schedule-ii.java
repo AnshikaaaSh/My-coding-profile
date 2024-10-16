5f5ce3e0671150d5631ce12f3eb260195caaad99
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-10-16 19:58:45 +0530

    Time: 18 ms (7.75%), Space: 45.7 MB (60.82%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void fun(int[] indeg, int[][] pre){
+        for(int i=0;i<pre.length;i++) indeg[pre[i][0]]++;
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indeg=new int[numCourses];
+        fun(indeg, prerequisites);
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0;i<indeg.length;i++){
+            if(indeg[i]==0) q.add(i);
+        }
+
+        ArrayList<Integer> arr=new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            arr.add(curr);
+            for(int i=0;i<prerequisites.length;i++){
+                if(prerequisites[i][1]==curr){
+                    indeg[prerequisites[i][0]]--;
+                    if(indeg[prerequisites[i][0]]==0) q.add(prerequisites[i][0]);
+                }
+            }
+        }
+        if(arr.size()!=numCourses) return new int[0];
+
+        int[] res=new int[arr.size()];
+
+        for(int i=0;i<arr.size();i++){
+            res[i]=arr.get(i);
+        }
+        return res;
+    }
+}
