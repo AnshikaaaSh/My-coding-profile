e9575c5eb1f58280384a45a991565ac3c86e88d6
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-11-09 23:25:36 +0530

    Time: 1 ms (78.68%), Space: 45.6 MB (50.26%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,13 +1,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //ArrayList<ArrayList<Integer>> al=new ArrayList<ArrayList<>>();
         int n=matrix.length;
         int m=matrix[0].length;
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         al.get(i).add(j);
-        //     }
-        // }
         int[] rows=new int[n];
         int[] col=new int[m];
 
@@ -23,7 +17,6 @@ class Solution {
             for(int j=0;j<m;j++){
                 if(rows[i]==1||col[j]==1){
                     matrix[i][j]=0;
-                    //al.get(i).set(j,0);
                 }
             }
         }
