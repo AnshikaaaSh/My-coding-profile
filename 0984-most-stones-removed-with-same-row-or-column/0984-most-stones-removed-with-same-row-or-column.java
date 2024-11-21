b6003987474f24973f6118482eaa29031228696f
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-11-21 18:13:42 +0530

    Time: 4 ms (97.25%), Space: 44.8 MB (64.26%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,36 @@
+class Solution {
+    int n, parent[];
+
+    public int removeStones(int[][] stones) {
+        n = stones.length;
+        parent = new int[20001];
+
+        for (int i = 0 ; i < parent.length ; i++)
+            parent[i] = i;
+
+        for (int [] stone : stones) {
+            int x = stone[0];
+            int y = stone[1] + 10001;  //to avoid index collisions
+            union(x,y);
+        }
+
+        Set<Integer> components = new HashSet<>();
+
+        for (int [] stone : stones)
+            components.add(find(stone[0]));
+
+        return n - components.size();
+    }
+
+    public void union(int i, int j) {
+        int parI = find(i);
+        int parJ = find(j);
+        parent[parI] = parJ;
+
+    }
+    
+    public int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    } 
+}
