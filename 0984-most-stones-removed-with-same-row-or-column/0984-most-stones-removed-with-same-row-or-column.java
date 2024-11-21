0528d08e1ed678e0a28c776071fe518585a99f85
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-11-21 18:14:01 +0530

    Time: 8 ms (87.81%), Space: 45.6 MB (6.67%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -1,36 +1,32 @@
 class Solution {
-    int n, parent[];
 
-    public int removeStones(int[][] stones) {
-        n = stones.length;
-        parent = new int[20001];
+    int islands = 0;
+    Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0 ; i < parent.length ; i++)
-            parent[i] = i;
-
-        for (int [] stone : stones) {
-            int x = stone[0];
-            int y = stone[1] + 10001;  //to avoid index collisions
-            union(x,y);
+    private int find(int stone) {
+        if (map.putIfAbsent(stone, stone) == null) {
+            islands++;
         }
 
-        Set<Integer> components = new HashSet<>();
-
-        for (int [] stone : stones)
-            components.add(find(stone[0]));
-
-        return n - components.size();
+        if (map.get(stone) != stone) {
+            map.put(stone, find(map.get(stone)));
+        }
+        return map.get(stone);
     }
 
-    public void union(int i, int j) {
-        int parI = find(i);
-        int parJ = find(j);
-        parent[parI] = parJ;
-
+    private void union(int stone1, int stone2) {
+        stone1 = find(stone1);
+        stone2 = find(stone2);
+        if (stone1 != stone2) {
+            map.put(stone1, stone2);
+            islands--;
+        }
     }
-    
-    public int find(int x) {
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    } 
+
+    public int removeStones(int[][] stones) {
+        for (int i = 0; i < stones.length; i++) {
+            union(stones[i][0], ~stones[i][1]);
+        }
+        return stones.length - islands;
+    }
 }
