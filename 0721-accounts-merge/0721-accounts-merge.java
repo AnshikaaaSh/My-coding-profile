5c16622ebcc051cbbb98713a75710b89bb949dcc
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-11-22 22:43:30 +0530

    Time: 29 ms (81.78%), Space: 47.6 MB (65.79%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,75 +1,54 @@
 class Solution {
+
     int[] parent;
     int[] rank;
-    
-    public int find(int x) {
-        if (x == parent[x]) return x;
-        parent[x] = find(parent[x]);
+    public int find(int x){
+        if(x==parent[x]) return x;
+        parent[x]=find(parent[x]);
         return parent[x];
     }
-    
-    public void union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        
-        if (rank[px] > rank[py]) {
-            parent[py] = px;
-            rank[px]++;
-        } else {
-            parent[px] = py;
-            rank[py]++;
+    public void union(int u, int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+        else{
+            parent[pu]=pv;
+            rank[pv]++;
         }
     }
-    
-    
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        
-        int n = accounts.size();
-        
-        parent = new int[n];
-        rank = new int[n];
-        
-        for (int i = 0 ; i < n ; i++) parent[i] = i;
-        
-        Map<String, Integer> mailMap = new HashMap<>();
-        
-        for (int i = 0 ; i < n ; i++) {
-            
-            int m = accounts.get(i).size();
-            
-            for (int j = 1 ; j < m ; j++) {
-                
-                String mailAddress = accounts.get(i).get(j);
-                
-                if (!mailMap.containsKey(mailAddress)) {
-                    mailMap.put(mailAddress, i);
-                } else {
-                    union(i, mailMap.get(mailAddress)); // unioning ith index with the index where we encountered the same mail earlier.
-                }
+        int n=accounts.size();
+        parent=new int[n];
+        rank=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        Map<String,Integer> mailMap=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int m=accounts.get(i).size();
+            for(int j=1;j<m;j++){
+                String mailadd=accounts.get(i).get(j);
+                if(!mailMap.containsKey(mailadd)) mailMap.put(mailadd,i);
+                else union(i,mailMap.get(mailadd));
             }
         }
-        
-        
-        Map<Integer, Set<String>> distinctGroupMap = new HashMap<>();
-        
-        for (int i = 0 ; i < n ; i++) {
-            parent[i] = find(i); // further updating the parent in case of anyting left earlier
-            
-            int accountSize = accounts.get(i).size();
-            distinctGroupMap.putIfAbsent(parent[i], new HashSet<>());
-            distinctGroupMap.get(parent[i]).addAll(accounts.get(i).subList(1, accountSize)); // removing 0th element which is name
+        Map<Integer, Set<String>> distinctgm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            parent[i]=find(i);
+            int accsize=accounts.get(i).size();
+            distinctgm.putIfAbsent(parent[i], new HashSet<>());
+            distinctgm.get(parent[i]).addAll(accounts.get(i).subList(1,accsize));
         }
-        
-        List<List<String>> mergedAccounts = new ArrayList<>();
-        
-        for (int group : distinctGroupMap.keySet()) {
-            List<String> emailList = new ArrayList<>(distinctGroupMap.get(group));
+        List<List<String>> ans=new ArrayList<>();
+        for(int g:distinctgm.keySet()){
+            List<String> emailList=new ArrayList<>(distinctgm.get(g));
             Collections.sort(emailList);
-            
-            emailList.add(0, accounts.get(group).get(0)); // adding the name to the first element of the merged account
-            mergedAccounts.add(emailList);
+            emailList.add(0,accounts.get(g).get(0));
+            ans.add(emailList);
         }
-        
-        return mergedAccounts;
+        return ans;
     }
 }
