2d07abbe11aba616fd5e5d5fc5fa71095a26a416
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2024-11-23 10:14:51 +0530

    Time: 98 ms (59.78%), Space: 74.3 MB (72.46%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -1,105 +1,65 @@
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
-        }
-    }
-
-    public int findUPar(int node) {
-        if (node == parent.get(node)) {
-            return node;
-        }
-        int ulp = findUPar(parent.get(node));
-        parent.set(node, ulp);
-        return parent.get(node);
-    }
-
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
-        }
-    }
-
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-        }
-    }
-}
 class Solution {
-    private boolean isValid(int newr, int newc, int n) {
-        return newr >= 0 && newr < n && newc >= 0 && newc < n;
+    int island_id;
+    int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
+
+    public int dfs(int[][] grid, int r, int c) {
+        int m = grid.length;
+
+        if (0 <= r && r < m && 0 <= c && c < m && grid[r][c] == 1) {
+            grid[r][c] = island_id;
+            int a = 1;
+            for (int[] d : dir) {
+                int nR = r + d[0];
+                int nC = c + d[1];
+                a += dfs(grid, nR, nC);
+            }
+            return a;
+        } else {
+            return 0;
+        }
+
     }
 
-    public int largestIsland(int grid[][]) {
-        int n = grid.length;
-        DisjointSet ds = new DisjointSet(n * n);
-        // step - 1
-        for (int row = 0; row < n ; row++) {
-            for (int col = 0; col < n ; col++) {
-                if (grid[row][col] == 0) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n) && grid[newr][newc] == 1) {
-                        int nodeNo = row * n + col;
-                        int adjNodeNo = newr * n + newc;
-                        ds.unionBySize(nodeNo, adjNodeNo);
-                    }
+    public int largestIsland(int[][] grid) {
+        // 1st pass to update ids using dfs helper, create a mapping of ids with areas
+        island_id = -1;
+        int m = grid.length;
+        int n = grid[0].length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int area = 0;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (grid[r][c] == 1) {
+                    area = dfs(grid, r, c);
+                    map.put(island_id, area);
+                    island_id--;
                 }
+
             }
         }
-        // step 2
-        int mx = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 1) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                HashSet<Integer> components = new HashSet<>();
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n)) {
-                        if (grid[newr][newc] == 1) {
-                            components.add(ds.findUPar(newr * n + newc));
+        int max_area = Integer.MIN_VALUE;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (grid[r][c] == 0) {
+                    Set<Integer> seen = new HashSet<>();
+                    int a = 1;
+                    for (int[] d : dir) {
+                        int nR = r + d[0];
+                        int nC = c + d[1];
+
+                        if (0 <= nR && nR < m && 0 <= nC && nC < n && grid[nR][nC] != 0) {
+                            seen.add(grid[nR][nC]);
                         }
                     }
+
+                    for (int id : seen) {
+                        a += map.get(id);
+                    }
+                    max_area = Math.max(max_area, a);
+
                 }
-                int sizeTotal = 0;
-                for (Integer parents : components) {
-                    sizeTotal += ds.size.get(parents);
-                }
-                mx = Math.max(mx, sizeTotal + 1);
             }
         }
-        for (int cellNo = 0; cellNo < n * n; cellNo++) {
-            mx = Math.max(mx, ds.size.get(ds.findUPar(cellNo)));
-        }
-        return mx;
+        return max_area == Integer.MIN_VALUE ? m * n : max_area;
     }
-
 }
