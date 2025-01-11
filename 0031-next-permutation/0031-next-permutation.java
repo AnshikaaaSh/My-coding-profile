6a7cbdd4389e02d4e39ce9c371e7778118139b93
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2025-01-12 01:02:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (64.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,31 +1,23 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length, i = n - 2;
-        
-        while (i >= 0 && nums[i] >= nums[i + 1]) 
-            i--;
-        
-        if (i >= 0) {
-            int j = n - 1;
-
-            while (nums[j] <= nums[i]) 
-                j--;
-
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+        int n=nums.length, i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
+        if(i>=0){
+            int j=n-1;
+            while(nums[j]<=nums[i])j--;
+            int t=nums[i];
+            nums[i]=nums[j];
+            nums[j]=t;
         }
-
-        reverse(nums, i + 1, n - 1);
+        rev(nums,i+1,n-1);
     }
-    
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+    public void rev(int[] nums, int s, int e){
+        while(s<e){
+            int t=nums[s];
+            nums[s]=nums[e];
+            nums[e]=t;
+            s++;
+            e--;
         }
     }
 }
