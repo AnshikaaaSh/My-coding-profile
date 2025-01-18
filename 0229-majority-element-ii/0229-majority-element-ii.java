dca1a5937bf3f1b6bdcacdb4775522d08d0156a0
Anshika Sharma <85951434+AnshikaaaSh@users.noreply.github.com>
2025-01-18 14:08:56 +0530

    Time: 2 ms (99.24%), Space: 48.2 MB (38.84%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,52 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length/3;
-        HashMap<Integer, Integer> hm=new HashMap<>();
-        List<Integer> res=new ArrayList<>();
-        for(int i=0;i<nums.length;i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i],0)+1);
+        //array can at max have only 2 ele greater than n/3 times at a time
+        List<Integer> result= new ArrayList<>();
+        int n= nums.length;
+        int count1 =0;
+        int count2=0;
+        int candidate1=Integer.MIN_VALUE;
+        int candidate2=Integer.MIN_VALUE;
+
+        for(int num: nums){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+            else if(count1 ==0){
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if(count2 ==0){
+                candidate2 = num;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-        for(Map.Entry<Integer, Integer> entry: hm.entrySet()){
-            int el=entry.getKey();
-            int c=entry.getValue();
-            if(c>n) res.add(el);
+        //verify the two candidiates are correct or not
+        count1=0;
+        count2=0;
+
+        for(int num: nums){
+            if(num ==candidate1){
+                count1++;
+            }
+            else if(num ==candidate2){
+                count2++;
+            } 
         }
-        return res;
+
+        if(count1> n/3){
+            result.add(candidate1);
+        }
+        if(count2> n/3){
+            result.add(candidate2);
+        }
+        return result;
     }
 }
